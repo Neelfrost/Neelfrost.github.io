ee152007fe9e306a6f98525ef8a21f1929c88348
Neel Basak <neelfrost@gmail.com>
2021-11-18 13:52:46 +0530

    feat: theme change transition

--- js/dark_mode_toggle.js
+++ js/dark_mode_toggle.js
@@ -1,23 +1,34 @@
+// Elements
+var body = document.body;
+var nav = document.getElementsByTagName("nav")[0];
 var button = document.getElementById("dark-mode-toggle");
 var icon = document.getElementById("dark-mode-toggle-icon");
 
+// Theme storage
 var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
 var isDark = savedTheme == "dark" ? true : false;
 setTheme(savedTheme);
 
 button.addEventListener("click", () => {
+    // Add transition before changing theme
+    nav.classList.add("transition");
+    body.classList.add("transition");
+    // Apply theme
     isDark = !isDark;
-    if (isDark) setTheme("dark");
-    else setTheme("light");
+    if (isDark) {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
 });
 
 function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
-        document.body.classList.add("darktheme");
+        body.classList.add("darktheme");
         icon.className = "fas fa-sun";
     } else if (mode === "light") {
-        document.body.classList.remove("darktheme");
+        body.classList.remove("darktheme");
         icon.className = "fas fa-moon";
     }
 }
